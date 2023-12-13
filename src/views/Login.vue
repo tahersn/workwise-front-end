51326687105ee674ea69771e95f110fb0bbdf27b
Taher <taher.snoussi@esprit.tn>
2023-12-13 15:44:28 +0100

    login vue complete

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,21 +4,37 @@
             <img src="../assets/logo.svg" alt="logo" class="h-20 w-20">
         </div>
         <div class="relative flex flex-col float-right w-2/3 bg-white rounded-l-xl h-full items-center justify-center">
-            <img src="../assets/ordinateur.jpg" class="absolute left-[-120px] w-46 h-[200px] rounded-2xl" alt="">
+            <img src="../assets/ordinateur.jpg" class="absolute left-[-120px] w-[220px] h-[200px] rounded-3xl" alt="">
 
             <div class="flex items-center justify-center ">
                 <p class="text-4xl font-extrabold text-[#E6C3B0] pt-12 ">Sign-In</p>
             </div>
-            <div class="flex outline outline-offset-1 outline-orange-500 rounded-lg mt-5 mb-14 h-5/6 w-1/3">
+            <div class="flex outline outline-offset-1 outline-orange-500 rounded-lg mt-5 mb-14 h-fits w-1/3">
                 <div class="from-control flex flex-col items-start p-6 w-full">
-                <label for="name" class="text-[#383D42] font-bold text-[28px] float-left">Username</label>
-                <input id="name" type="text" class="rounded-md bg-[#EEEEEE] text-[#A2A2A2] w-full p-2 my-2 outline-orange-500" placeholder="What's your name" />
+                    <label for="name" class="text-[#383D42] font-bold text-[28px] float-left">Username</label>
+                    <input id="name" type="text" class="rounded-md bg-[#EEEEEE] text-[#A2A2A2] w-full p-2 my-2 outline-orange-500" placeholder="What's your name" />
 
-                <label for="Pass" class="text-[#383D42] font-bold text-[28px] float-left">Password</label>
-                <input id="Pass" type="password" class="rounded-md bg-[#EEEEEE] text-[#A2A2A2] w-full p-2 my-2 outline-orange-500" placeholder="What's your name" />
+                    <label for="Pass" class="text-[#383D42] font-bold text-[28px] float-left">Password</label>
+                    <input id="Pass" type="password" class="rounded-md bg-[#EEEEEE] text-[#A2A2A2] w-full p-2 my-2 outline-orange-500" placeholder="********" />
 
+                    <div class="flex flex-col w-full p-2">
+                    <p class="font-bold text-[#383D42] pb-2">Forgot password ?</p>
+                    <p class="font-bold text-[#383D42] pb-2"> Register</p>
+                    </div>
+                    <div class="flex flex-row justify-between w-full px-6 py-2">
+                        <div>
+                            <img src="../assets/logogg.svg" height="64px" width="64px" alt="">
+                        </div>
+                        <div>
+                            <img src="../assets/logofb.svg" height="64px" width="64px" alt="">
+                        </div>
+
+                    </div>
                 </div>
+                
+               
             </div>
+            
 
         </div>
             
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,8 @@
 <template>
+    //navbar
     <p class="boler text-center text-red-800">This is the main page</p>
     <router-view/>
+    //sidebar
 </template>
 
 <script>
